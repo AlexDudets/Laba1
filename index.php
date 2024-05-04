4de97e70c86e94b301d84fe1e3502abe9c9992d3
1.9TDI <domcasach1@gmail.com>
2024-05-04 16:09:18 +0000

    ДА

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
-$name = $_GET['name'];
-if (isset($name)) {
-    
-    echo "Привет, " . htmlspecialchars($name ). "!";
-} else {
-    
-    echo "Ошибка";  
-}
-;
+if(isset($_GET["num"])) {
+    $num = (int)$_GET["num"]; 
+    if ($num < 1000)
+    {
+        for ($i = 2; $i <= $num; $i++) {
+            $is_prime = true; 
+        
+            for ($j = 2; $j < $i; $j++) {
+                if ($i % $j === 0) {
+                    $is_prime = false; 
+                    break; 
+                }
+            }
 
+            if ($is_prime) {
+                echo $i . " ";
+            }
+        }
+    }
+} elseif(isset($_GET["name"])) {
+    $name = htmlspecialchars($_GET["name"]);
+    echo 'Привет, ' . $name . '!';
+}
+?>
